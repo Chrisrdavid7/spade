cfefe2f748d13044103621f4bf3430e3dc4daa5a
“Chris <chrisrdavid7@gmail.com>
2025-08-18 17:20:46 -0400

    Ok

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,20 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Sitemap from 'vite-plugin-sitemap'
+import { ViteSitemapPlugin } from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     vue(),
-    Sitemap({
-      hostname: 'https://spadedj.com',
-      // simple list of routable paths
-      urls: [
-        '/',
+    ViteSitemapPlugin({
+      hostname: 'https://allspade.com',
+      routes: [
+        '/', 
         '/about',
-        '/tour',
         '/bookings',
-        '/press', 
-        '/shop', 
+        '/tour',
+        '/press',
+        '/shop'
       ]
-      // you can also pass a function to fetch Vite router routes automatically
     })
   ]
 })
