36d752d1baa9a68a2d201a749ed6d3510047425d
“Chris <chrisrdavid7@gmail.com>
2025-08-19 15:09:48 -0400

    Ok

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import viteSitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    viteSitemap({
-      hostname: 'https://allspade.com',
-      dynamicRoutes: ['/', '/about', '/bookings', '/tour', '/press', '/shop'],
-      generateRobotsTxt: false   // 👈 disable robots.txt creation
-    }),
-  ],
+  plugins: [vue()]
 })
